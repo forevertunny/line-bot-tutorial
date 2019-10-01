fd91213248e6d1755703f52955e7291c58c7615c
forevertunny <forevertunny@gmail.com>
2019-10-01 23:05:06 +0800

    111

--- app.py
+++ app.py
@@ -347,6 +347,7 @@ def test2(message):
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print("user_id: ", event.source.user_id)
     print("event.reply_token:", event.reply_token)
     print("event.message.text:", event.message.text)
 

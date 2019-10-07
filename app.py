d73537d528ab58ff750ae5e4825ffcd5b992bdb8
forevertunny <forevertunny@gmail.com>
2019-10-07 21:34:05 +0800

    fff

--- app.py
+++ app.py
@@ -502,7 +502,10 @@ def handle_message(event):
             event.reply_token, image_message)
         return 0
     if event.message.text.lower() == "test3":
-        test3()
+        content =  test3()
+        line_bot_api.reply_message(
+            event.reply_token, 
+            TextSendMessage(text=content)))
         return 0
     if event.message.text.lower() =='redinfo' or event.message.text.lower() =='紅信':
         return 0
